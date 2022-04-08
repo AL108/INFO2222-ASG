685fc202adf304f1498f5530d66907b794400cf0
Ali <alie0302@uni.sydney.edu.au>
2022-04-08 10:13:27 +1000

    got ssl to work

--- src/run.py
+++ src/run.py
@@ -46,7 +46,7 @@ def run_server():
         run_server
         Runs a bottle server
     '''
-    run(host=host, port=port, debug=debug)
+    run(host=host, port=port,debug=debug, keyfile='/home/alien/certs/127.0.0.1-key.pem', certfile='/home/alien/certs/127.0.0.1.pem', server="gunicorn")
 
 #-----------------------------------------------------------------------------
 # Optional SQL support
