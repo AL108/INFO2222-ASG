f25f38aac82440fe95c8fe190f486ba3d0e2a14e
Ali <alieberman108@gmail.com>
2022-05-16 14:20:38 +1000

    bug fix with id generator

--- src/ID_generator.py
+++ src/ID_generator.py
@@ -1,9 +1,12 @@
+def lines_in_file(filename):
+    return len(open(filename,"r").read().split("\n"))
+
 class ID_generator:
     # an instance of this class can generate unique ids
     def __init__(self):
         self.char_index = 0
         self.chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVW!@#$%^&*()abcdefghijklmnopqrstuvwxyz"
-        self.id = [""]
+        self.id = [str(lines_in_file("./db/forums.txt") + lines_in_file("./db/posts.txt"))]
     
     def generate_id(self):
         if self.char_index == 0:
